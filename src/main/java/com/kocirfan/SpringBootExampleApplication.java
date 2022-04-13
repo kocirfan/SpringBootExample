0832146c919ae139e5f627d2c635d48758b7a1ba
kocirfan <irfnk83@gmail.com>
2022-04-14 02:40:04 +0300

    ModelMapperBean ve PasswordEncoderBean

--- src/main/java/com/kocirfan/SpringBootExampleApplication.java
+++ src/main/java/com/kocirfan/SpringBootExampleApplication.java
@@ -3,6 +3,7 @@ package com.kocirfan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
--- src/main/java/com/kocirfan/dto/BaseDto.java
+++
@@ -1,9 +0,0 @@
-package com.kocirfan.dto;
-
-import java.util.Date;
-
-public class BaseDto {
-
-    private Long id;
-    private Date date;
-}
