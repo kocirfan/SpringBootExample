e742853886e70459dbb1f68471d45388174bbcef
kocirfan <irfnk83@gmail.com>
2022-04-14 02:52:40 +0300

    EmployeeDto

---
+++ src/main/java/com/kocirfan/dto/EmployeeDto.java
@@ -0,0 +1,21 @@
+package com.kocirfan.dto;
+//DTO: Data Transfer Object ==> Middleware kısmında kullanılacak
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
+public class EmployeeDto {
+
+    private Long id;
+    private String firsName;
+    private String lastName;
+    private String emailId;
+}
