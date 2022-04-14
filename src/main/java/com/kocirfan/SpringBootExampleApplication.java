d18c2d40323b15657bc740d6117641f8e9bc8c7a
kocirfan <irfnk83@gmail.com>
2022-04-15 01:09:01 +0300

    Auditing (kim ne zaman ekledi-güncelledi)

--- src/main/java/com/kocirfan/SpringBootExampleApplication.java
+++ src/main/java/com/kocirfan/SpringBootExampleApplication.java
@@ -2,12 +2,18 @@ package com.kocirfan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
 )
+
+//Auditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+
 public class SpringBootExampleApplication {
 
     public static void main(String[] args) {
