a5aa124d7e498d792059ace0dd119ead1edd471b
kocirfan <irfnk83@gmail.com>
2022-04-15 03:49:29 +0300

    Json Web Token success bearertoken

--- src/main/java/com/kocirfan/SpringBootExampleApplication.java
+++ src/main/java/com/kocirfan/SpringBootExampleApplication.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-       org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
-)
-
+//@SpringBootApplication(exclude = {
+//        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+//       org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+//)
+@SpringBootApplication
 //Auditing
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 
---
+++ src/main/java/com/kocirfan/security/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.kocirfan.security;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+public class WebSecurityConfig  {
+
+//@EnableWebSecurity
+// public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http
+//                .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic();
+//    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+//        authenticationManagerBuilder
+//                .inMemoryAuthentication()
+//                .withUser("irfankocaksoy")
+//                .password("{noop}root")
+//                .roles("USER");
+//    }
+
+//    @Autowired
+//    public void configureGlobalEncryped(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+//      PasswordEncoder passwordEncoder= PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//
+//
+//        authenticationManagerBuilder
+//                .inMemoryAuthentication()
+//                .withUser("irfankocaksoy")
+//                .password(passwordEncoder.encode("root"))
+//                .roles("USER");
+//    }
+// }
+
+
+}
